3e5f979baa0b15ba672ede5689434bcdb90240ba
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-10-11 21:58:09 -0400

    some TLC, it’s an API now

--- bluline-interface.py
+++ bluline-interface.py
@@ -1,26 +1,30 @@
 import requests as r
-#import tkinter as tk
 import xmltodict as xml
-import time as t
-import getpass as gp
 
-print("BluLog CLI")
+def getDetails(username, password):
+    return xml.parse(r.get(f"https://http-receiver.bluconsole.com/bluconsolerest/1.0/resources/devices?uname={username}&upass={password}").content)
 
-uname = input("Email/username: ")
-upass = gp.getpass()
+if __name__ == "__main__":
+    import time as t
+    import getpass as gp
 
-d = xml.parse(r.get(f"https://http-receiver.bluconsole.com/bluconsolerest/1.0/resources/devices?uname={uname}&upass={upass}").content)
-temps = []
-names = []
-times = []
+    print("BluLog CLI")
 
-try:
-    for dev in d["devices"]["tdl"]:
-        temps.append(dev["ms"]["m"]["t"])
-        names.append(dev["label"])
-        times.append(t.strftime("%x %X", t.localtime(int(dev["ms"]["m"]["utc"]))))
-    print("Current temps:")
-    for ti in range(len(temps)):
-        print(f"{names[ti]} (as of {times[ti]}) - {temps[ti]}°C")
-except:
-    print("Invalid username or password!")
+    uname = input("Email/username: ")
+    upass = gp.getpass()
+
+    d = getDetails(uname, upass)
+    temps = []
+    names = []
+    times = []
+
+    try:
+        for dev in d["devices"]["tdl"]:
+            temps.append(dev["ms"]["m"]["t"])
+            names.append(dev["label"])
+            times.append(t.strftime("%x %X", t.localtime(int(dev["ms"]["m"]["utc"]))))
+        print("Current temperatures:")
+        for ti in range(len(temps)):
+            print(f"{names[ti]} (as of {times[ti]}) - {temps[ti]}°C")
+    except:
+        print("Invalid username or password!")
